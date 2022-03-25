dcf7ee30cdf2dca25fd97e321d0bc9a1e8c2afc2
Alexandra Jaramillo <dahianajaramillo157@gmail.com>
2022-03-25 08:44:20 -0500

    Configurando el juego y creando el menu

--- src/alex/sofka/reto/Main.java
+++ src/alex/sofka/reto/Main.java
@@ -1,18 +1,54 @@
 package alex.sofka.reto;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
+    private static final String[] banco_de_preguntas = {"geografia.txt", "historia.txt", "matematica.txt", "fisica.txt","politica.txt"};
+
     public static void main(String[] args) {
 
+        boolean play = true;
+        Scanner in = new Scanner(System.in);
+        int opt;
+
+        while (play){
+            imprimirMenu();
+            opt = in.nextInt();
+            switch (opt){
+                case 1:
+                    playTest();
+                    break;
+                case 2:
+                    play = false;
+                    System.out.println("Hasta pronto, vuelve!!");
+                    break;
+                default:
+                    System.out.println("Ingrese una opcion correcta");
+                    break;
+            }
+        }
+
+    }
+
+    public static void imprimirMenu(){
+        System.out.println("Bienvenido A preguntas y Respuestas");
+        System.out.println("1. Jugar");
+        System.out.println("2. Exit");
+        System.out.println("Selecciona una opcion:");
+
+    }
+
+    public static void playTest(){
         Test t = new Test();
 
         try{
-            t.loadData("preguntas-correcto.txt");
+            t.loadData(banco_de_preguntas[(int) Math.floor(Math.random()*(4 +1)+0)]);
             t.makeTest();
         } catch (IOException ex){
             System.out.println(ex.getMessage());
         }
     }
+
 }
